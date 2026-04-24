2f497fb970618a0129d38a25d8062ddeedeb187a
Aflah Shadil k <shadilpml@gmail.com>
2026-04-24 12:47:48 +0530

    modified main

--- lib/main.dart
+++ lib/main.dart
@@ -6,12 +6,15 @@ import 'routes/app_routes.dart';
 import 'controllers/auth_controller.dart';
 import 'services/notification_service.dart';
 import 'services/sync_service.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   
   // Initialize Firebase
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   // Initialize Services
   await Get.putAsync(() => NotificationService().init());
